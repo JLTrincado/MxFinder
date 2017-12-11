446a1ff36ff77422137113f1f0703498449e7e65
JLTrincado <juanluis.trincado@upf.edu>
2017-12-11 15:10:53 -0500

    Modified check_introns_3UTR.py for outputing also genes without introns in different list

--- check_introns_3UTR.py
+++ check_introns_3UTR.py
@@ -31,9 +31,13 @@ def main():
 
         gtf_path = sys.argv[1]
         output_path = sys.argv[2]
+        output_path = sys.argv[3]
+        #
 
         # gtf_path = "/home/shinoda/Desktop/Florida/annotation/refseq_hg19_full.formatted.gtf"
         # output_path = "/home/shinoda/Desktop/Florida/annotation/genes_introns_3UTR.tab"
+        # output_path2 = "/home/shinoda/Desktop/Florida/annotation/genes_no_introns_3UTR.tab"
+
 
         # 1. Get all the information for each gene
         transcript_gene, transcripts_exons, transcripts_CDS, transcripts_strand, transcripts_stop_codon = {}, {}, {}, {}, {}
@@ -47,14 +51,10 @@ def main():
                 gene_id = str(tokens[8].split(";")[0].split("gene_id")[1])[2:-1]
                 #Save the association transcript - gene
                 transcript_gene[transcript_id] = gene_id
-                # if(transcript_id in transcript_gene):
-                #     transcript_gene[transcript_id].append(gene_id)
-                # else:
-                #     transcript_gene[transcript_id] = [gene_id]
                 # Save the strand
                 if(transcript_id in transcripts_strand):
                     if(transcripts_strand[transcript_id]!=tokens[6]):
-                        raise Exception("Oposite strands associated to the same transcript: "+transcript_id)
+                        raise Exception("Opposite strands associated to the same transcript: "+transcript_id)
                     else:
                         pass
                 else:
@@ -65,22 +65,9 @@ def main():
                         transcripts_exons[transcript_id].append([tokens[3],tokens[4]])
                     else:
                         transcripts_exons[transcript_id] = [[tokens[3],tokens[4]]]
-                # # Save the CDS cordinates
-                # if(tokens[2]=="CDS"):
-                #     if (transcript_id in transcripts_CDS):
-                #         transcripts_CDS[transcript_id].append([tokens[3],tokens[4]])
-                #     else:
-                #         transcripts_CDS[transcript_id] = [[tokens[3],tokens[4]]]
                 # Save the stop codon cordinates
                 if(tokens[2]=="stop_codon"):
                     if (transcript_id in transcripts_stop_codon):
-                        # raise Exception("There are 2 stop codons associated to the same transcript: "+transcript_id)
-                        # logger.info("There are 2 stop codons associated to the same transcript: " + transcript_id)
-                        # logger.info("New stop_codon: "+tokens[3]+"-"+tokens[4])
-                        # logger.info("Old stop_codon: "+transcripts_stop_codon[transcript_id])
-                        # strand = tokens[6]
-                        # culo = transcripts_stop_codon[transcript_id].split("-")[1]
-                        # culo2 = transcripts_stop_codon[transcript_id].split("-")[0]
                         if(tokens[6]=="+" and tokens[4]>transcripts_stop_codon[transcript_id][1]):
                             transcripts_stop_codon[transcript_id] = transcripts_stop_codon[transcript_id][0]+"-"+tokens[4]
                         elif(tokens[6]=="-" and tokens[3]<transcripts_stop_codon[transcript_id][0]):
@@ -88,7 +75,7 @@ def main():
                     else:
                         transcripts_stop_codon[transcript_id] = [tokens[3],tokens[4]]
 
-        # 2. Sort the exons and the CDS cordinates, depending on the strand
+        # 2. Sort the exons cordinates, depending on the strand
         logger.info("Sorting exons...")
         for key,values in transcripts_exons.items():
             if(transcripts_strand[key] == "+"):
@@ -98,9 +85,10 @@ def main():
 
         # 3. Check for each transcript_id if the after the stop_codon there are more exons
         logger.info("Checking the stop codon localizations...")
-        list_genes = []
+        list_genes_introns, list_genes_no_introns = [], []
         for key, values in transcripts_exons.items():
             flag_stop_codon = False
+            gene_id = transcript_gene[key]
             if(key in transcripts_stop_codon):
                 stop_codon = transcripts_stop_codon[key]
                 for element in values:
@@ -109,31 +97,46 @@ def main():
                         if((not flag_stop_codon) and (element[0]<=transcripts_stop_codon[key][1] and
                         element[1]>=transcripts_stop_codon[key][1])):
                             flag_stop_codon = True
+                            #If we have reached the last exon, put it in the list of genes with no introns in 3UTR
+                            if(element[0]==values[-1][0] and element[1]==values[-1][1]):
+                                if (gene_id not in list_genes_no_introns):
+                                    list_genes_no_introns.append(gene_id)
+                                break
                         elif(flag_stop_codon):
                             # There are introns in the 3UTR region. Output this gene
-                            gene_id = transcript_gene[key]
-                            if(gene_id not in list_genes):
-                                list_genes.append(gene_id)
+                            if(gene_id not in list_genes_introns):
+                                list_genes_introns.append(gene_id)
                             break
                     else:
                         if ((not flag_stop_codon) and (element[0] <= transcripts_stop_codon[key][0] and
                                                                element[1] >= transcripts_stop_codon[key][0])):
                             flag_stop_codon = True
+                            #If we have reached the last exon, put it in the list of genes with no introns in 3UTR
+                            if(element[0]==values[-1][0] and element[1]==values[-1][1]):
+                                if (gene_id not in list_genes_no_introns):
+                                    list_genes_no_introns.append(gene_id)
+                                break
                         elif (flag_stop_codon):
                             # There are introns in the 3UTR region. Output this gene
-                            gene_id = transcript_gene[key]
-                            if(gene_id not in list_genes):
-                                list_genes.append(gene_id)
+                            if(gene_id not in list_genes_introns):
+                                list_genes_introns.append(gene_id)
                             break
 
-        # 4. Output the list of genes sorted
-        list_genes_sorted = sorted(list_genes)
+        # 4. Output the list of genes sorted with introns
+        list_genes_introns_sorted = sorted(list_genes_introns)
         outFile = open(output_path, 'w')
-        for x in list_genes_sorted:
+        for x in list_genes_introns_sorted:
             outFile.write("\""+x+"\""+"\n")
-
         outFile.close()
         logger.info("Generated "+output_path)
+
+        # 5. Output the list of genes sorted without introns
+        list_genes_no_introns_sorted = sorted(list_genes_no_introns)
+        outFile = open(output_path2, 'w')
+        for x in list_genes_no_introns_sorted:
+            outFile.write("\""+x+"\""+"\n")
+        outFile.close()
+        logger.info("Generated "+output_path2)
 
         logger.info("Done. Exiting program.")
 
