c3f641380b302d99a9b860010c021d36bd435ef9
JLTrincado <juanluis.trincado@upf.edu>
2017-12-15 18:27:26 -0500

    Added more files to lib/

---
+++ lib/get_distance_to_ss.py
@@ -0,0 +1,188 @@
+"""
+@authors: Juan L. Trincado
+@email: juanluis.trincado@upf.edu
+
+get_distance_to_ss.py: Given the ORFs per transcript (take the longest per transcript),
+the sequences of each transcript and the position of the ss, get the relative distance to this ss
+If the distance returned is 0, that means the stop codon is falling in the final exon and the transcript
+is not going into NMD. If the distance is between 1 and 50, the stop codon is not falling in the last
+exon but is not going to NMD. If its greater than 50, thn it goes to NMD
+"""
+
+import logging, sys, os, re
+from copy import deepcopy
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create console handler and set level to info
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+def get_distance_to_ss(orfs_path, transcripts_paths, output_path):
+    try:
+
+        # orfs_path = sys.argv[1]
+        # transcripts_paths = sys.argv[2]
+        # output_path = sys.argv[3]
+        #
+        # orfs_path = "/home/shinoda/Desktop/Florida/annotation/MBNL1_TEST/MBNL3_possible_ORFs_refseq.fa"
+        # transcripts_paths = "/home/shinoda/Desktop/Florida/annotation/MBNL1_TEST/MBNL3_possible_transcripts_refseq.fa.paths"
+        # output_path = "/home/shinoda/Desktop/Florida/annotation/MBNL1_TEST/MBNL3_evaluated_refseq.paths"
+
+        # 1. Extract just the first ORF (the longest) from each transcript
+        outFile = open(orfs_path+".unique", 'w')
+        with open(orfs_path) as f:
+            logger.info("Processing ORFs file...")
+            transcript_id, transcript_id_old = "Transcript_0", ""
+            for line in f:
+                # If its header
+                if(re.search(">", line)):
+                    #If the transcript id changes, output the next 2 lines
+                    if(not(re.search(transcript_id, line))):
+                        tokens = line.rstrip().split(":")
+                        transcript_id = tokens[0][1:]
+                        pass_flag = False
+                        outFile.write(line)
+                    else:
+                        pass_flag = True
+                # If its sequence
+                else:
+                    if(pass_flag):
+                        pass
+                    else:
+                        outFile.write(line)
+        outFile.close()
+
+        # 2. Using the info in the header of the ORFs unique file, we can obtain the information of
+        # whether our stop codon is falling
+        results = {}
+        with open(orfs_path+".unique") as f:
+            logger.info("Processing ORFs unique file...")
+            for line in f:
+                # If its header
+                if (re.search(">", line)):
+                    # Two things: obtain the differences for each exon associated (with the cordinates)
+                    # and obtain the length of the ORF
+                    tokens = line.rstrip().split(":")
+                    strand = tokens[1]
+                    transcript_id = tokens[0][1:]
+                    exons = tokens[2:-1]
+                    ORF = tokens[-1].split("-")
+                    # Get the difference for each exon. Go substracting the sequences according to the distance
+                    # to the ORF end
+                    first_pos = int(ORF[0])
+                    second_pos = int(ORF[1])
+                    pos = 0
+                    flag_1, flag_2, flag_3 = True, False, False
+                    i = 0
+                    for x in exons:
+                        coords = x.split("_")
+                        length_exon = int(coords[1]) - int(coords[0])
+                        if (flag_1):
+                            # coords = x.split("_")
+                            # length_exon = int(coords[1]) - int(coords[0])
+                            if((pos + length_exon) < first_pos):
+                                pass
+                                # pos += length_exon
+                            # Reached start codon
+                            else:
+                                offset = first_pos - pos
+                                if(strand=="+"):
+                                    start_codon = int(coords[0]) + offset
+                                else:
+                                    start_codon = int(coords[1]) - offset
+                                flag_1 = False
+                                flag_2 = True
+                                if ((pos + length_exon) < second_pos):
+                                    pass
+                                    # pos += length_exon
+                                # Reached stop codon
+                                else:
+                                    # offset = second_pos - (pos + length_exon)
+                                    offset = second_pos - pos
+                                    if (strand == "+"):
+                                        stop_codon = int(coords[0]) + offset
+                                    else:
+                                        stop_codon = int(coords[1]) - offset
+                                    flag_2 = False
+                                    flag_3 = True
+                                    # pos += length_exon
+                                    # if (i == len(exons) - 1):
+                                    #     distance = pos - second_pos
+                                    # else:
+                                    #     pass
+                        if (flag_2):
+                            # coords = x.split("_")
+                            # length_exon = int(coords[1]) - int(coords[0])
+                            if ((pos + length_exon) < second_pos):
+                                pass
+                                # pos += length_exon
+                            # Reached stop codon
+                            else:
+                                offset = second_pos - pos
+                                if (strand == "+"):
+                                    stop_codon = int(coords[0]) + offset
+                                else:
+                                    stop_codon = int(coords[1]) - offset
+                                flag_2 = False
+                                flag_3 = True
+                                # pos += length_exon
+                                # If reached last exon, the stop codon is in the last exon. Therefore, there won't be NMD
+                                # if (i==len(exons)-1):
+                                #     # distance = (pos + length_exon) - second_pos
+                                #     distance = 0
+                                #     break
+                                # else:
+                                #     pass
+                        if (flag_3):
+                            # If not reached last exon, get the distance to the 3'ss
+                            if(i!=len(exons)-1):
+                                # coords = x.split("_")
+                                # length_exon = int(coords[1]) - int(coords[0])
+                                # pos += length_exon
+                                distance = (pos + length_exon) - second_pos
+                                break
+                            # If reached last exon, the stop codon is in the last exon. Therefore, there won't be NMD
+                            else:
+                                distance = 0
+                                break
+                        pos += length_exon
+                        i += 1
+                    results[transcript_id] = [start_codon,stop_codon,distance]
+                else:
+                    pass
+
+        # 3. Associate the info to the transcripts_paths file
+        outFile = open(output_path, 'w')
+        with open(transcripts_paths) as f:
+            logger.info("Processing transcripts_paths file...")
+            for line in f:
+                tokens = line.rstrip().split(":")
+                transcript_id = tokens[0][1:]
+                #Get the information from the previous results
+                if(transcript_id in results):
+                    info = results[transcript_id]
+                    outFile.write(":".join(tokens)+"\tStart_codon: "+str(info[0])+
+                                  "\tStop_codon: "+str(info[1])+
+                                  "\tDistance: "+str(info[2])+"\n")
+                else:
+                    outFile.write(":".join(tokens)+"\tNo ORF\n")
+        outFile.close()
+        logger.info("Saved " + output_path)
+
+    except Exception as error:
+        logger.error('ERROR: ' + repr(error))
+        logger.error("Aborting execution")
+        sys.exit(1)
---
+++ lib/test_NMDs.py
@@ -0,0 +1,160 @@
+"""
+@authors: Juan L. Trincado
+@email: juanluis.trincado@upf.edu
+
+test_NMDs.py: The following script do a Fisher test for each pair of exons, in order to test
+independence between all the pairs of exons respect to the NMD condition.
+Output: p_value of the Fisher_test and odds_ratio. The greater the odds_ratio the greater the association
+of coocurrence with nonNMD. The smaller the odds_ratio the greater the association of coocurrence with nonNMD
+(therefore, there is mutual exclusion)
+"""
+
+import logging, sys, os, re
+import scipy.stats
+from statsmodels.sandbox.stats.multicomp import multipletests
+from itertools import combinations
+from copy import deepcopy
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# create console handler and set level to info
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+def test_NMDs(paths_file, output_path):
+    try:
+
+        # paths_file = sys.argv[1]
+        # output_path = sys.argv[2]
+        #
+        # paths_file = "/home/shinoda/Desktop/Florida/annotation/MBNL1_TEST/MBNL1_evaluated_refseq.paths"
+        # output_path = "/home/shinoda/Desktop/Florida/annotation/MBNL1_TEST/MBNL1_evaluated_p_value_refseq.paths"
+
+        # 1. Get the whole list of exons in the file
+        exons_list = [] #list of exons
+        with open(paths_file) as f:
+            logger.info("Getting the whole list of exons in the file...")
+            for line in f:
+                tokens = line.rstrip().split("\t")
+                if(tokens[1]!="No ORF"):
+                    aux_list = re.split('\W+', tokens[0])[2:-1]
+                    #Transform all elements to integer
+                    aux_list_int = list(map(int, aux_list))
+                    for x in aux_list_int:
+                        #Put the exon in the exons_lis if it's not already
+                        if(x not in exons_list):
+                            exons_list.append(x)
+
+        # 2. Read again the file, count by pairs how many times appears depending of the NMD
+        exon_counts = {} # counts of NMD or not NMD per exon_pair, and coocurrence
+        # (e.g: [2,3] -> [coocurrence:[NMD,noNMD], nocoocurrence:[NMD,noNMD]]
+        with open(paths_file) as f:
+            logger.info("Counting ocurrences of exon pairs...")
+            for line in f:
+                tokens = line.rstrip().split("\t")
+                if(tokens[1]!="No ORF"):
+                    if(int(re.split('\W+', tokens[3])[1])<50):
+                        NMD_flag = False
+                    else:
+                        NMD_flag = True
+                    aux_list = re.split('\W+', tokens[0])[2:-1]
+                    # Transform all elements to integer
+                    aux_list_int = list(map(int, aux_list))
+                    # Get all the combinations for exons_list
+                    combinations_list = list(combinations(exons_list, 2))
+                    # Check if each pair show up in the transcript
+                    for x in combinations_list:
+                        #coocurrence
+                        if((x[0] in aux_list_int and x[1] in aux_list_int) or
+                               (x[0] not in aux_list_int and x[1] not in aux_list_int)):
+                            if(x not in exon_counts):
+                                #NMD
+                                if(NMD_flag):
+                                    exon_counts[x] = [[0,0],[1,0]]
+                                #no NMD
+                                else:
+                                    exon_counts[x] = [[0,0],[0,1]]
+                            else:
+                                #NMD
+                                if(NMD_flag):
+                                    exon_counts[x][1][0] += 1
+                                #no NMD
+                                else:
+                                    exon_counts[x][1][1] += 1
+                        # no coocurrence
+                        else:
+                            if(x not in exon_counts):
+                                #NMD
+                                if(NMD_flag):
+                                    exon_counts[x] = [[1,0],[0,0]]
+                                #no NMD
+                                else:
+                                    exon_counts[x] = [[0,1],[0,0]]
+                            else:
+                                #NMD
+                                if(NMD_flag):
+                                    exon_counts[x][0][0] += 1
+                                #no NMD
+                                else:
+                                    exon_counts[x][0][1] += 1
+
+
+        # 3. Get all the combinations between pairs of exons, and do a fisher test.
+        # Save the p-values for applying an FDR correction
+        outFile = open(output_path+".temp", 'w')
+        outFile.write("Pair_exons\todds_ratio\tp_value\tcontingency_table\n")
+        raw_pvals = []
+        for key, values in exon_counts.items():
+            #Do a fisher test with the associated values
+            oddsratio, pvalue = scipy.stats.fisher_exact([values[0],values[1]])
+            raw_pvals.append(pvalue)
+            #Output the results
+            outFile.write(str(key)+"\t"+str(oddsratio)+"\t"+str(pvalue)+"\t["+
+                          str(values[0])+","+str(values[1])+"]\n")
+        outFile.close()
+
+        # 4. Apply the FDR correction and output it in the file
+        _, pvals_corrected, _, _ = multipletests(raw_pvals, method='fdr_bh', alpha=0.05)
+        pvals_corrected_list = pvals_corrected.tolist()
+        outFile = open(output_path, 'w')
+        outFile.write("Pair_exons\todds_ratio\tp_value\tFDR\tcontingency_table\n")
+        cont = 0
+        with open(output_path+".temp") as f:
+            next(f)
+            logger.info("Applying multiple testing correction...")
+            for line in f:
+                tokens = line.rstrip().split("\t")
+                outFile.write(tokens[0]+"\t"+tokens[1]+"\t"+tokens[2]+"\t"+str(pvals_corrected_list[cont])+"\t"+tokens[3]+"\n")
+                cont += 1
+        outFile.close()
+
+        # 5. Sort the output file
+        outFile = open(output_path+".sorted", 'w')
+        lines = open(output_path, 'r').readlines()[1:]
+        outFile.write("Pair_exons\todds_ratio\tp_value\tFDR\tcontingency_table\n")
+        for line in sorted(lines, key=lambda line: float(line.split("\t")[2])):
+            outFile.write(line)
+        outFile.close()
+
+        # Remove the auxiliary file
+        os.remove(output_path)
+        os.remove(output_path+".temp")
+
+        logger.info("Saved " + output_path+".sorted")
+
+    except Exception as error:
+        logger.error('ERROR: ' + repr(error))
+        logger.error("Aborting execution")
+        sys.exit(1)
